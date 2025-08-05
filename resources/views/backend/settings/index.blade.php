876e8a69a63b3bf23769421e8cd6442acc149161
Moeen Uddin <m.moeen.u9@gmail.com>
2025-08-05 16:28:40 +0600

    Update: Setting Index for Backend backend>settings>index

--- resources/views/backend/settings/index.blade.php
+++ resources/views/backend/settings/index.blade.php
@@ -57,45 +57,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="card card-large-icons">
-                        <div class="card-icon bg-primary text-white">
-                            <i class="fas fa-power-off"></i>
-                        </div>
-                        <div class="card-body">
-                            <h4>System</h4>
-                            <p>PHP version settings, time zones and other environments.</p>
-                            <a href="features-setting-detail.html" class="card-cta">Change Setting <i
-                                    class="fas fa-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card card-large-icons">
-                        <div class="card-icon bg-primary text-white">
-                            <i class="fas fa-lock"></i>
-                        </div>
-                        <div class="card-body">
-                            <h4>Security</h4>
-                            <p>Security settings such as firewalls, server accounts and others.</p>
-                            <a href="features-setting-detail.html" class="card-cta">Change Setting <i
-                                    class="fas fa-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card card-large-icons">
-                        <div class="card-icon bg-primary text-white">
-                            <i class="fas fa-stopwatch"></i>
-                        </div>
-                        <div class="card-body">
-                            <h4>Automation</h4>
-                            <p>Settings about automation such as cron job, backup automation and so on.</p>
-                            <a href="features-setting-detail.html" class="card-cta text-primary">Change Setting <i
-                                    class="fas fa-chevron-right"></i></a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
