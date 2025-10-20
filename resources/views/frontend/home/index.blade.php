56ead66f8bce84973b80b2c378e0f5ab00a799e2
Moeen Uddin <m.moeen.u9@gmail.com>
2025-10-20 18:26:53 +0600

    Update: Quick view button design emplements

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -80,12 +80,15 @@
                                     <span>$60.00</span>
                                 </div>
 
-                                <div class="pro-icon">
-                                    <ul>
-                                        <a class="trigger" href="#" data-id="{{ $product->id }}" title="Quick View" role="button">
-                                            Quick View
-                                        </a>
-                                    </ul>
+                                <div class="icon-wrapper">
+                                    <div class="pro-icon">
+                                        <ul>
+                                            <a class="trigger" href="#" data-id="{{ $product->id }}" title="Quick View"
+                                                role="button">
+                                                Quick View
+                                            </a>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
